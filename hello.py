815f98739b6dc10086cd973d72650f53a78df555
Mayday_ys <yuans1993@126.com>
2017-07-11 15:53:46 +0800

    add flash

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from flask_wtf import Form
-from wtforms import StringField,SubmitField
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 
 app = Flask(__name__)
@@ -28,17 +28,14 @@ def internal_server_error(e):
 
 @app.route('/',methods=['GET','POST'])
 def index():
-	name = None
 	form = NameForm()
 	if form.validate_on_submit():
-		name = form.name.data
-		form.name.data = ''
-	return render_template('index.html', form=form, name=name)
-
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
-
+		old_name = session.get('name')
+		if old_name is not None and old_name != form.name.data:
+			flash('Looks like you have changed your name!')
+		session['name'] = form.name.data
+		return redirect(url_for('index'))
+	return render_template('index.html', form=form, name=session.get('name'))
 
 if __name__ == '__main__':
     manager.run()
